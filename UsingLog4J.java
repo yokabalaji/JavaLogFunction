8da459f632b5f3e2e6d1ae46df3521077a2886d3
yokabalaji <tigo1515007@gmail.com>
2022-07-01 12:45:15 +0530

    springboot

---
+++ UsingLog4J.java
@@ -0,0 +1,17 @@
+package com.example;
+
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.Logger;
+
+public class UsingLog4J {
+	static Logger log=Logger.getLogger(UsingLog4J.class);
+	public static void main(String[] args) {
+		BasicConfigurator.configure();
+		log.info("hi");
+		log.warn("hello");
+		log.error("welcome");
+		log.fatal("to");
+		log.debug("chennai");
+			
+	}
+}
---
+++ UsingLog4JApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsingLog4JApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsingLog4JApplication.class, args);
+	}
+
+}
